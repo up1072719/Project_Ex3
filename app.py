7b51089d7a3216f61b2c77b10a702b82d12a2e61
up1072719 <134546228+up1072719@users.noreply.github.com>
2023-05-29 01:28:29 +0300

    Add files via upload
    First Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import paho.mqtt.client as mqtt
+
+app = Flask(__name__)
+broker = "localhost"
+topic = "mytopic"
+
+@app.route('/', methods=['POST'])
+def handle_post():
+    text = request.get_data(as_text=True)
+
+    # Publish data via MQTT
+    client = mqtt.Client()
+    client.connect(broker, 1883)
+    client.publish(topic, text)
+    client.disconnect()
+
+    return "Your text published via MQTT: " + text
+
+@app.route('/', methods=['GET'])
+def handle_get():
+    return "This route only accepts POST requests."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
